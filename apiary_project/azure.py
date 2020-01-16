7daf2a22cb27ca01c873e8eb2d6e479f5ecdd3b4
wshar <wshar@bmtdt.com.au>
2020-01-16 16:29:50 +1100

    first commit

---
+++ apiary_project/azure.py
@@ -0,0 +1,21 @@
+from .settings import *
+
+DEBUG = False
+DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+AZURE_ACCOUNT_NAME = os.getenv('AZ_STORAGE_ACCOUNT_NAME')
+AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
+AZURE_ACCOUNT_KEY = os.getenv('AZ_STORAGE_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('APP_DB_NAME'),
+        'USER': os.getenv('POSTGRES_ADMIN_USER'),
+        'PASSWORD': os.getenv('POSTGRES_ADMIN_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
+    }
+}
+
