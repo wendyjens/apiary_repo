4eff9ba220d2a88a932d7a1456bb0e38f4607982
Wendy Shar <wshar@bmtdt.com.au>
2020-01-16 17:33:19 +1100

    Added all environment variables into azure.py

--- apiary_project/azure.py
+++ apiary_project/azure.py
@@ -7,6 +7,25 @@ AZURE_ACCOUNT_NAME = os.getenv('AZ_STORAGE_ACCOUNT_NAME')
 AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
 AZURE_ACCOUNT_KEY = os.getenv('AZ_STORAGE_KEY')
 
+AZ_GROUP='wshar_rg_Linux_australiaeast'
+AZ_LOCATION='australiaeast'
+
+APP_SERVICE_APP_NAME='apiarydev'
+
+POSTGRES_SERVER_NAME='django.db.backends.postgresql'
+POSTGRES_ADMIN_USER='qgvvzgpx'
+POSTGRES_ADMIN_PASSWORD='lJn3plV_Qp4KA-8woHA1y0yLxEjGQmif'
+
+POSTGRES_HOST='rosie.db.elephantsql.com'
+
+APP_DB_NAME='qgvvzgpx'
+
+DJANGO_SETTINGS_MODULE='apiary_project.azure'
+
+AZ_STORAGE_ACCOUNT_NAME='apiarydevstorage'
+AZ_STORAGE_KEY='n2fI+fanSIHjlLOltt/mqMviq1S9c9fJ7g+g8/r3b4eLzuRLWiB50JE46IBy2yAqh38N35daN5cpF3ET8tc+HQ=='
+AZ_STORAGE_CONTAINER='apiarydevstoragecontainer'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
